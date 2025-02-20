de9e1218bde7a2064b656fef4ad86977aed7cf64
YouSiki <you.siki@outlook.com>
2025-02-20 15:36:41 +0800

    更新_worker.js，添加lscr.io到主机路由映射

--- _worker.js
+++ _worker.js
@@ -19,6 +19,7 @@ function routeByHosts(host) {
 		"ghcr": "ghcr.io",
 		"cloudsmith": "docker.cloudsmith.io",
 		"nvcr": "nvcr.io",
+		"lscr": "lscr.io",
 
 		// 测试环境
 		"test": "registry-1.docker.io",
